e301a3a493a87262abde6ab606fed7d70aed2076
Madhawa2001 <avishka123madhawa@gmail.com>
2025-04-05 15:52:26 +0530

    cmt1

---
+++ AI Chatbot using python from scratch/main.py
@@ -0,0 +1,61 @@
+import sys
+print("Running from:", sys.executable)
+
+import os 
+import json
+import random
+import nltk
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+class ChatbotModel(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(ChatbotModel, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+
+class ChatbotAssistant:
+
+    def __init__(self, intents_path, funciton_mappings = None):
+        self.model = None
+        self.intents_path = intents_path
+
+        self.documents = []
+        self.vocabulary = []
+        self.intents = []
+        self.intents_responses = []
+        
+        self.function_mappings = funciton_mappings
+        
+        self.X = None
+        self.y = None
+
+    @staticmethod
+    def tokenize_and_lemmaize(text):
+        lemmatizer = nltk.WordNetLemmatizer()
+
+        words = nltk.word_tokenize(text)
+        words = [lemmatizer.lemmatize(word.lower()) for word in words]
+
+        return words
+
+chatbot = ChatbotAssistant('intents.josn')
+print(chatbot.tokenize_and_lemmaize('Hello world how are you, I am progeamming in python today'))
+
